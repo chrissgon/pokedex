b6caa6975b93b338b6a8270481bbbd9066757ba2
Christopher Gonçalves <christopher.goncalves2002@hotmail.com.br>
2022-11-01 10:22:50 -0300

    feat(header): create header of page
    Create header, implement darkmode and add background of page

--- app.vue
+++ app.vue
@@ -1,5 +1,26 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div :class="{ dark: dark }">
+    <section class="App min-h-screen dark:bg-slate-900 p-6">
+      <!-- header -->
+      <header class="relative w-full flex justify-center">
+        <img width="125" src="/pokemon.svg" />
+
+        <DarkMode @change="changeDarkMode" />
+      </header>
+    </section>
   </div>
 </template>
+
+<script setup lang="ts">
+const dark = useCookie<boolean>("dark");
+
+function changeDarkMode(isDark: boolean) {
+  dark.value = isDark;
+}
+</script>
+
+<style scoped>
+:not(.dark) > .App {
+  background: linear-gradient(to bottom, #fff, #eee, #eee, #fff);
+}
+</style>
