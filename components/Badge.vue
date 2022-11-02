1ef7839c9b4a435f1971d22c380f978449001ba9
Christopher Gonçalves <christopher.goncalves2002@hotmail.com.br>
2022-11-01 22:49:09 -0300

    feat(badge): create badge component

---
+++ components/Badge.vue
@@ -0,0 +1,19 @@
+<template>
+  <p
+    :style="{
+      background: color,
+    }"
+    class="inline-flex rounded-full px-3 py-1 cursor-pointer text-sm text-white"
+  >
+    <slot></slot>
+  </p>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  color: string;
+}
+defineProps<Props>();
+</script>
+
+<style scoped></style>
