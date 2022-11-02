840c3bfa892537c301542646534ab76979dfd7a3
Christopher Gonçalves <christopher.goncalves2002@hotmail.com.br>
2022-11-01 22:48:29 -0300

    feat(inputtext): create input text component

---
+++ components/InputText.vue
@@ -0,0 +1,22 @@
+<template>
+  <input
+    @input="
+      $emit('update:modelValue', ($event.target as HTMLInputElement).value)
+    "
+    :value="modelValue"
+    type="text"
+    class="mt-16 w-full max-w-xs h-12 pl-6 rounded-full text-base font-medium outline-none dark:bg-slate-800 "
+  />
+</template>
+
+<script setup lang="ts">
+interface Props {
+  modelValue: any;
+}
+
+defineProps<Props>();
+
+defineEmits(["update:modelValue"]);
+</script>
+
+<style scoped></style>
