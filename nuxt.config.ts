776fbee3817834771627284bf896d6d5a765ac13
Christopher Gonçalves <christopher.goncalves2002@hotmail.com.br>
2022-11-02 06:03:40 -0300

    chore(favicon): add favicon in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,10 @@ export default defineNuxtConfig({
         rel: "stylesheet",
         href: "https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap",
       },
+      {
+        rel: "icon",
+        href: "/pokebola.png",
+      },
     ],
   },
 
